00bfb42ede5b4adf761249925beece50ea84a4ad
MonkieeBoi <danny.pham26425@gmail.com>
2025-03-25 14:48:24 +1100

    feat: show popup on map click

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,14 +1,34 @@
-var map = L.map("map").setView([-33.9173534616886, 151.23126985972831], 17);
+let map = L.map("map", {
+  zoom: 17,
+  center: L.latLng(-33.9173534616886, 151.23126985972831),
+  maxBounds: L.latLngBounds(
+    L.latLng(-33.86692728515977, 151.17067658807477),
+    L.latLng(-33.94499762920698, 151.28045227050784),
+  ),
+});
 
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
+  minZoom: 14,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-map.on('click', function(e) {
-    console.log(e.latlng.lat);
-    console.log(e.latlng.lng);
-    popup = document.getElementById("popup");
-    popup.classList.toggle("hidden");
-})
+let popup = L.popup();
+let latlng = null;
+
+map.on("click", function (e) {
+  latlng = e.latlng;
+  popup
+    .setLatLng(e.latlng)
+    .setContent(`Add flare <button @click="popup=true">✅<button>`)
+    .openOn(map);
+});
+
+// navigator.geolocation.getCurrentPosition(
+//   (pos) => {
+//     let mark = L.marker([pos.coords.latitude, pos.coords.longitude]);
+//     mark.bindTooltip("You are here").openTooltip();
+//     mark.addTo(map);
+//   },
+// );
