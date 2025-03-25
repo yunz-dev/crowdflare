aa5d88c7a9e86adb7f7c714c929b6a686b9a75e7
MonkieeBoi <danny.pham26425@gmail.com>
2025-03-25 16:30:58 +1100

    feat: htmx call on flare click

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -21,9 +21,37 @@ map.on("click", function (e) {
   latlng = e.latlng;
   popup
     .setLatLng(e.latlng)
-    .setContent(`Add flare <button @click="popup=true">✅<button>`)
+    .setContent(`<button @click="popup=true">Add flare 🎆<button>`)
     .openOn(map);
 });
+
+dummy_flares = [
+  { id: "1", lat: -33.91717944990013, lng: 151.2298107147217 },
+  { id: "2", lat: -33.91724622440734, lng: 151.23012721538547 },
+  { id: "3", lat: -33.917406483011156, lng: 151.2306743860245 },
+];
+
+for (const flare of dummy_flares) {
+  let marker = L.circle([flare.lat, flare.lng]);
+  marker.setStyle({
+    fillColor: "red",
+    stroke: false,
+    fillOpacity: 0.4,
+    radius: 200,
+  });
+  marker.addTo(map);
+
+  e = marker._path;
+  if (!e) {
+    continue;
+  }
+
+  e.setAttribute("hx-get", "/component/flare");
+  e.setAttribute("hx-vals", `{"id": ${flare.id}}`);
+  e.setAttribute("hx-target", "#popup");
+  e.setAttribute("x-on:click", "popup=true");
+  htmx.process(e);
+}
 
 // navigator.geolocation.getCurrentPosition(
 //   (pos) => {
