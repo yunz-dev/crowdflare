c41ddc285eeafd4797760f88367d82d0282a8c80
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-25 21:30:19 +1100

    feat: make models for flare and change models for leaderboard and user

--- internal/models/leaderboard.go
+++ internal/models/leaderboard.go
@@ -1,6 +1,7 @@
 package models
 
 type LeaderboardData struct {
+	CurrentUser User	`bson:current_user`
 	TopUsers []User		`bson:top_users`
 	Contributors []User	`bson:contributors`
 }
--- internal/models/user.go
+++ internal/models/user.go
@@ -1,6 +1,10 @@
 package models
 
 type User struct {
-    Username  string `bson:"username"`
-    Karma int    `bson:"karma"`
+    Username  string        `bson:"username"`
+    Streak int              `bson:"streak"`
+    FlareCount int          `bson:"contribution_count"`
+    FlareHistory []Flare    `bson:"contribution_history"`
+    Karma int               `bson:"karma"`
+    KarmaHistory []int      `bson:"karma_history"`
 }
