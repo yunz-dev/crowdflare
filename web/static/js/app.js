de47049f8f50dafc5353f6531ed8897ff1fca599
MonkieeBoi <danny.pham26425@gmail.com>
2025-03-26 11:01:59 +1100

    feat: coloured icons

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -32,6 +32,39 @@ map.on("click", (e) => {
 //   { id: "4", lat: -33.91726403093376, lng: 151.22993677854538 },
 //   { id: "5", lat: -33.91751371612105, lng: 151.23352171604117 },
 // ];
+icons = {
+    0: L.icon({
+          iconUrl: "/static/icons/map-marker-0.svg",
+            iconSize: [40, 40],
+            popupAnchor: [0, -20]
+        }),
+    1: L.icon({
+          iconUrl: "/static/icons/map-marker-1.svg",
+            iconSize: [40, 40],
+            popupAnchor: [0, -20]
+        }),
+    2: L.icon({
+          iconUrl: "/static/icons/map-marker-2.svg",
+            iconSize: [40, 40],
+            popupAnchor: [0, -20]
+        }),
+    3: L.icon({
+          iconUrl: "/static/icons/map-marker-3.svg",
+            iconSize: [40, 40],
+            popupAnchor: [0, -20]
+        }),
+    4: L.icon({
+          iconUrl: "/static/icons/map-marker-4.svg",
+            iconSize: [40, 40],
+            popupAnchor: [0, -20]
+        }),
+    5: L.icon({
+          iconUrl: "/static/icons/map-marker-5.svg",
+            iconSize: [40, 40],
+            popupAnchor: [0, -20]
+        }),
+}
+
 fetch("/api/flares")
   .then((response) => response.json())
   .then((json) => {
@@ -41,6 +74,7 @@ fetch("/api/flares")
     for (const flare of json) {
       let marker = L.marker([flare.Lat, flare.Lng], {
         bubblingMouseEvents: false,
+        icon: icons[flare.Rating]
       });
       marker.addTo(map);
       popup = L.popup(null, { maxWidth: 800 });
